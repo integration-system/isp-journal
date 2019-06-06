9abf605a53e1dea913c1c1bd180ea3e729d4af5b
ykomissarov <merser1q@gmail.com>
2019-06-06 14:11:36 +0300

    init

--- transfer/isp_journal_transfer.go
+++ transfer/isp_journal_transfer.go
@@ -1,18 +1,31 @@
 package transfer
 
 import (
+	"fmt"
+	"github.com/integration-system/isp-journal/entry"
 	"github.com/integration-system/isp-journal/log"
 	"github.com/integration-system/isp-lib/backend"
 	"github.com/integration-system/isp-lib/logger"
 	"github.com/integration-system/isp-lib/streaming"
 	"google.golang.org/grpc/metadata"
+	"time"
 
 	"os"
 )
 
 const (
-	transferMethod = "isp-journal/log/transfer"
+	transferMethod = "journal/log/transfer"
+
+	moduleNameField = "moduleName"
+	createdAtField  = "createdAt"
+	hostField       = "host"
 )
+
+type LogInfo struct {
+	ModuleName string
+	Host       string
+	CreatedAt  time.Time
+}
 
 func TransferAndDeleteLogFile(client *backend.RxGrpcClient) func(file log.LogFile) {
 	return func(log log.LogFile) {
@@ -32,6 +45,34 @@ func TransferAndDeleteLogFiles(client *backend.RxGrpcClient) func(logs []log.Log
 	}
 }
 
+func GetLogInfo(bf streaming.BeginFile) (*LogInfo, error) {
+	moduleName, err := bf.FormData.GetStringValue(moduleNameField)
+	if err != nil {
+		return nil, err
+	}
+
+	host, err := bf.FormData.GetStringValue(hostField)
+	if err != nil {
+		return nil, err
+	}
+
+	createdAt, err := bf.FormData.GetStringValue(createdAtField)
+	if err != nil {
+		return nil, err
+	}
+
+	createdAtTime, err := entry.ParserTime(createdAt)
+	if err != nil {
+		return nil, fmt.Errorf("invalid '%s' time format: %v", createdAtField, err)
+	}
+
+	return &LogInfo{
+		ModuleName: moduleName,
+		Host:       host,
+		CreatedAt:  createdAtTime,
+	}, nil
+}
+
 func doTransfer(client *backend.RxGrpcClient, f log.LogFile) {
 	err := client.Visit(func(c *backend.InternalGrpcClient) error {
 		return c.InvokeStream(transferMethod, -1, func(stream streaming.DuplexMessageStream, md metadata.MD) error {
@@ -48,11 +89,17 @@ func doTransfer(client *backend.RxGrpcClient, f log.LogFile) {
 	}
 }
 
-func statToFileHeader(f os.FileInfo) streaming.BeginFile {
+func statToFileHeader(f log.LogFile, moduleName, host string) streaming.BeginFile {
+	formData := map[string]interface{}{
+		moduleNameField: moduleName,
+		hostField:       host,
+		createdAtField:  entry.FormatTime(f.CreatedAt),
+	}
 	return streaming.BeginFile{
 		FileName:      f.Name(),
 		FormDataName:  f.Name(),
 		ContentType:   "application/binary",
 		ContentLength: f.Size(),
+		FormData:      formData,
 	}
 }
