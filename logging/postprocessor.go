119a65aa29929e1c7e33bcb6cd79b305d140c26c
ykomissarov <merser1q@gmail.com>
2019-06-10 14:14:27 +0300

    add postprocessor

--- logging/postprocessor.go
+++ logging/postprocessor.go
@@ -6,20 +6,27 @@ import (
 	"github.com/integration-system/isp-lib/logger"
 )
 
-func WithLogging(journal journal.Journal, enable bool) backend.PostProcessor {
+func WithLogging(journal journal.Journal, enable bool, includeMethods ...string) backend.PostProcessor {
+	includedMethods := make(map[string]bool, len(includeMethods))
+	for _, m := range includeMethods {
+		includedMethods[m] = true
+	}
 	return func(ctx backend.RequestCtx) {
 		if !enable {
 			return
 		}
 
-		err := ctx.Error()
-		if err != nil {
-			if err := journal.Error(ctx.Method(), ctx.RequestBody(), ctx.ResponseBody(), err); err != nil {
-				logger.Warnf("could not write to file journal: %v", err)
-			}
-		} else {
-			if err := journal.Info(ctx.Method(), ctx.RequestBody(), ctx.ResponseBody()); err != nil {
-				logger.Warnf("could not write to file journal: %v", err)
+		method := ctx.Method()
+		if include := includedMethods[method]; include {
+			err := ctx.Error()
+			if err != nil {
+				if err := journal.Error(method, ctx.RequestBody(), ctx.ResponseBody(), err); err != nil {
+					logger.Warnf("could not write to file journal: %v", err)
+				}
+			} else {
+				if err := journal.Info(method, ctx.RequestBody(), ctx.ResponseBody()); err != nil {
+					logger.Warnf("could not write to file journal: %v", err)
+				}
 			}
 		}
 	}
